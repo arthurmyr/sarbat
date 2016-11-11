c67c3b01aa4e7bfad89d03008f454b3fa8b60ccc
arthurmyr <arthurmeyer12@gmail.com>
2016-11-11 21:01:26 +0100

    Add sass config.rb plus 3 partials and 1 main scss file Update gitignore for .sass-cache folder Add all images of the PSD Add 5 sections tag to index.html
    	modified:   .gitignore
    	new file:   public/config.rb
    	deleted:    public/css/main.css
    	new file:   public/img/3docean-dark.png
    	new file:   public/img/activeden-dark.png
    	new file:   public/img/audiojungle-dark.png
    	new file:   public/img/codecanyon-dark.png
    	new file:   public/img/cover.png
    	new file:   public/img/grahpicriver-dark.png
    	new file:   public/img/icon_behance.png
    	new file:   public/img/icon_cofee-cup.png
    	new file:   public/img/icon_diamond.png
    	new file:   public/img/icon_facebook.png
    	new file:   public/img/icon_folder.png
    	new file:   public/img/icon_g+.png
    	new file:   public/img/icon_headset.png
    	new file:   public/img/icon_mouse.png
    	new file:   public/img/icon_pencil.png
    	new file:   public/img/icon_settings.png
    	new file:   public/img/icon_truck.png
    	new file:   public/img/icon_twitter.png
    	new file:   public/img/icon_wallet.png
    	new file:   public/img/icon_wrench.png
    	new file:   public/img/photodune-dark.png
    	new file:   public/img/quotation-mark.png
    	modified:   public/index.html
    	new file:   public/stylesheets/_fonts.scss
    	new file:   public/stylesheets/_reset.scss
    	new file:   public/stylesheets/_style.scss
    	new file:   public/stylesheets/main.scss

---
+++ public/config.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets = false
+line_comments = true
+sourcemap = false
+preferred_syntax = :scss
